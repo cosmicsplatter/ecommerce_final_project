bcdf5123d742cadba137393bd288d75b74730379
Mark Phillips <mphil27524@gmail.com>
2022-04-07 18:21:33 -0500

    Added basic user management with revise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :installs
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
   ActiveAdmin.routes(self)
-
-  root "products#index"
+  get "home/about"
+  root "home#index"
 
   resources :products
   resources :options
